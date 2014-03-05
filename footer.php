d94c7d032bae19f5a7826bb685a89849b9a0214c
toonya <msc5762@me.com>
2014-03-05 15:16:31 +0800

    use min.'s

--- footer.php
+++ footer.php
@@ -1,14 +1,13 @@
 
 		<footer>
 			<div class="container">
-				<?php echo esc_attr(get_option('footer_contact'));?>
+				<?php echo get_option('footer_contact');?>
 			</div>
 		</footer>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="http://code.jquery.com/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/js/masonry.pkgd.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/fn.js"></script>
     <?php wp_footer(); ?>
   </body>

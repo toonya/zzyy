7d175d62ec8d277fe143d612b2715e9263aee2ec
toonya <msc5762@me.com>
2014-03-05 15:31:41 +0800

    remove 150

--- js/fn.js
+++ js/fn.js
@@ -1,3 +1,12 @@
 jQuery(function($){
 	$('.current-menu-item').addClass('active');
+	$('.single .item img').each(function(i,e){
+			var imgSrc = $(this).attr('src');
+			var reg = '-150x150';
+			imgSrc = imgSrc.replace(reg,'');
+
+			$(this).attr('src',imgSrc);
+		});
+
+	})
 })
